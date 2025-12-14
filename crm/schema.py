48ed4009ddcec4900f36fc0f3bebd0e9e24c2a8e
mensahAmpomah <gideonmensah201@gmail.com>
2025-12-14 19:10:39 +0000

    Add the filtering

--- crm/schema.py
+++ crm/schema.py
@@ -5,7 +5,8 @@ from django.utils import timezone
 from graphene_django import DjangoObjectType
 
 from .models import Customer, Product, Order
-
+from graphene_django.filter import DjangoFilterConnectionField
+from .filters import CustomerFilter, ProductFilter, OrderFilter
 class CRMQuery:
     pass
 
@@ -194,3 +195,26 @@ class Query(CRMQuery, graphene.ObjectType):
 
     def resolve_orders(self, info):
         return Order.objects.all()
+    
+
+class CRMQueryWithResolvers(CRMQuery, graphene.ObjectType):
+    customers = graphene.List(CustomerType)
+    products = graphene.List(ProductType)
+    orders = graphene.List(OrderType)
+
+   
+    all_customers = DjangoFilterConnectionField(CustomerType, filterset_class=CustomerFilter)
+    all_products = DjangoFilterConnectionField(ProductType, filterset_class=ProductFilter)
+    all_orders = DjangoFilterConnectionField(OrderType, filterset_class=OrderFilter)
+
+    def resolve_customers(self, info):
+        return Customer.objects.all()
+
+    def resolve_products(self, info):
+        return Product.objects.all()
+
+    def resolve_orders(self, info):
+        return Order.objects.all()
+    
+
+Query = CRMQueryWithResolvers
