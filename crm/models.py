2148974157bbd51441cdf9f1b24b03fd65b72026
mensahAmpomah <gideonmensah201@gmail.com>
2025-12-14 19:01:33 +0000

    max length changed to 100

--- crm/models.py
+++ crm/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, blank=True, null=True)
+    phone = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
 
