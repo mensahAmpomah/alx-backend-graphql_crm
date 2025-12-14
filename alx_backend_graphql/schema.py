ec617f7ad3d091c4425c0ee75228f067ca3e997b
mensahAmpomah <gideonmensah201@gmail.com>
2025-12-14 16:22:28 +0000

    added the object

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,16 @@
 import graphene
 
-class Query(graphene.ObjectType):
+
+class CRMQuery:
+    """
+    Base query class for CRM-related queries.
+    (Can be extended later)
+    """
+    pass
+
+
+class Query(CRMQuery, graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
+
 
 schema = graphene.Schema(query=Query)
